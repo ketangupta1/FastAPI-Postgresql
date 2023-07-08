46f9d4e1971e72c74048967b03efe694d0b25e2f
Ketan Gupta <ketankumarnna@gmail.com>
2023-07-08 13:11:08 +0530

    finals

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import psycopg2
+from fastapi import HTTPException
 from config import *
 
 
@@ -10,24 +11,44 @@ conn = psycopg2.connect(
     password=PASSWORD
 )
 
+# Connect to the PostgreSQL database
+def connect_to_database():
+    try:
+        conn = psycopg2.connect(
+            host=HOST,
+            database=DB,
+            user=USER,
+            password=PASSWORD
+        )
+        return conn
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail="Error connecting to the database.") from e
 
-# Create a cursor object to execute SQL queries
-cur = conn.cursor()
+
 
 # Creating the source_data table
 # Since in /add_data endpoint request body doe not contains source_id. 
 # so I thought it should be auto generated so used SERIAL datatype for source_id
+
 def create_table():
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS source_data (
-            source_id SERIAL PRIMARY KEY,
-            source VARCHAR(200),
-            source_type VARCHAR(10),
-            source_tag VARCHAR(10),
-            last_update_date TIMESTAMP,
-            from_date TIMESTAMP ,
-            to_date TIMESTAMP,
-            frequency VARCHAR(5)
-        );
-    """)
-    conn.commit()
+    conn = connect_to_database()
+    try:
+        cur = conn.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS source_data (
+                source_id SERIAL PRIMARY KEY,
+                source VARCHAR(200) NOT NULL,
+                source_type VARCHAR(10) NOT NULL,
+                source_tag VARCHAR(10) NOT NULL,
+                last_update_date TIMESTAMP NOT NULL,
+                from_date TIMESTAMP NOT NULL,
+                to_date TIMESTAMP NOT NULL,
+                frequency VARCHAR(5) NOT NULL
+            );
+        """)
+        conn.commit()
+        cur.close()
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail="Error creating the table.") from e
+    finally:
+        conn.close()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 from datetime import datetime, timedelta
@@ -19,90 +19,137 @@ class SourceData(BaseModel):
     last_update_date: str
     frequency: str
 
+
+
 class UpdateSourceData(BaseModel):
     source_id: int
     from_date: str
     to_date: str
-    last_update_date: str
+    last_update_date: str 
 
 
 # API endpoint to get data for a given source
 @app.get("/get_data")
 def get_data(source_id: int) -> dict:
-    cur.execute("SELECT * FROM source_data WHERE source_id = %s", (source_id,))
-    rows = cur.fetchall()
-    if rows == []:
-        return {"Output":"Source Id does not exist"}
-    row = rows[0]
-    data = {"source_id":row[0],
-            "source":row[1],
-            "source_type":row[2],
-            "source_tag":row[3],
-            "last_update_date":row[4].strftime("%Y-%m-%d %H:%M:%S"),
-            "from_date":row[5].strftime("%Y-%m-%d %H:%M:%S"),
-            "to_date":row[6].strftime("%Y-%m-%d %H:%M:%S"),
-            "frequency":row[7]
-    }
-    return data
+    conn = connect_to_database()
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM source_data WHERE source_id = %s", (source_id,))
+        rows = cur.fetchall()
+        if rows == []:
+            return {"Output":"Source Id does not exist"}
+        row = rows[0]
+        data = {"source_id":row[0],
+                "source":row[1],
+                "source_type":row[2],
+                "source_tag":row[3],
+                "last_update_date":row[4].strftime("%Y-%m-%d %H:%M:%S"),
+                "from_date":row[5].strftime("%Y-%m-%d %H:%M:%S"),
+                "to_date":row[6].strftime("%Y-%m-%d %H:%M:%S"),
+                "frequency":row[7]
+        }
+        return data
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail="Error fetching data from the database.") from e
+    finally:
+        conn.close()
 
 
 
 # API endpoint to get data for a given source with updated from_date and to_date
 @app.get("/get_data_trigger")
 def get_data_trigger(source_id: int) -> dict:
-    cur.execute("SELECT * FROM source_data WHERE source_id = %s", (source_id,))
-    rows = cur.fetchall()
-    if rows == []:
-        return {"Output":"Source Id does not exist"}
-    row = rows[0]
-    frequency = int(row[7][:-1])
-    data = {"source_id":row[0],
-            "source":row[1],
-            "source_type":row[2],
-            "source_tag":row[3],
-            "last_update_date":row[4].strftime("%Y-%m-%d %H:%M:%S"),
-            "from_date":(row[5] + timedelta(minutes=frequency)).strftime("%Y-%m-%d %H:%M:%S"),
-            "to_date":(row[6] + timedelta(minutes=frequency)).strftime("%Y-%m-%d %H:%M:%S"),
-            "frequency":row[7]
-    }
-    return data
+    conn = connect_to_database()
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM source_data WHERE source_id = %s", (source_id,))
+        rows = cur.fetchall()
+        if rows == []:
+            return {"Output":"Source Id does not exist"}
+        row = rows[0]
+        frequency = int(row[7][:-1])
+        data = {"source_id":row[0],
+                "source":row[1],
+                "source_type":row[2],
+                "source_tag":row[3],
+                "last_update_date":row[4].strftime("%Y-%m-%d %H:%M:%S"),
+                "from_date":(row[5] + timedelta(minutes=frequency)).strftime("%Y-%m-%d %H:%M:%S"),
+                "to_date":(row[6] + timedelta(minutes=frequency)).strftime("%Y-%m-%d %H:%M:%S"),
+                "frequency":row[7]
+        }
+        return data
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail="Error Fetching data from the database.") from e
+    finally:
+        conn.close()
 
 
 
 # API endpoint to update from_date, to_date, and last_update_date
 @app.put("/update_data")
 def update_data(update_source_data: UpdateSourceData) -> dict:
-    # Convert date strings to datetime objects
-    from_date = datetime.strptime(update_source_data.from_date, "%Y-%m-%d %H:%M:%S")
-    to_date = datetime.strptime(update_source_data.to_date, "%Y-%m-%d %H:%M:%S")
-    last_update_date = datetime.strptime(update_source_data.last_update_date, "%Y-%m-%d %H:%M:%S")
+    try:
+        # Convert date strings to datetime objects
+        from_date = datetime.strptime(update_source_data.from_date, "%Y-%m-%d %H:%M:%S")
+        to_date = datetime.strptime(update_source_data.to_date, "%Y-%m-%d %H:%M:%S")
+        last_update_date = datetime.strptime(update_source_data.last_update_date, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Incorrect date format. Date format should be 'YYYY-MM-DD HH:MM:SS'.")
 
-    cur.execute(
-        "UPDATE source_data SET from_date = %s, to_date = %s, last_update_date = %s WHERE source_id = %s",
-        (from_date, to_date, last_update_date, update_source_data.source_id)
-    )
-    conn.commit()
-    return {"status": "success"}
-
+    conn = connect_to_database()
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM source_data WHERE source_id = %s", (update_source_data.source_id,))
+        rows = cur.fetchall()
+        if rows == []:
+            return {"Output":"Source Id does not exist"}
+        cur.execute(
+            "UPDATE source_data SET from_date = %s, to_date = %s, last_update_date = %s WHERE source_id = %s",
+            (from_date, to_date, last_update_date, update_source_data.source_id)
+        )
+        conn.commit()
+        return {"status": "success"}
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail="Error updating data in the database.") from e
+    finally:
+        conn.close()
 
 
 # API endpoint to add a new record
 @app.post("/add_data")
 def add_data(source_data: SourceData) -> dict:
-    # Convert date strings to datetime objects
-    from_date = datetime.strptime(source_data.from_date, "%Y-%m-%d %H:%M:%S")
-    to_date = datetime.strptime(source_data.to_date, "%Y-%m-%d %H:%M:%S")
-    last_update_date = datetime.strptime(source_data.last_update_date, "%Y-%m-%d %H:%M:%S")
-    cur.execute(
-        "INSERT INTO source_data (source, source_tag, source_type, from_date, to_date, last_update_date, frequency) "
-        "VALUES (%s, %s, %s, %s, %s, %s, %s)",
-        (source_data.source, source_data.source_tag, source_data.source_type, from_date, to_date, last_update_date, source_data.frequency)
-    )
-    conn.commit()
-    return {"status": "success"}
+    try:
+        # Convert date strings to datetime objects
+        from_date = datetime.strptime(source_data.from_date, "%Y-%m-%d %H:%M:%S")
+        to_date = datetime.strptime(source_data.to_date, "%Y-%m-%d %H:%M:%S")
+        last_update_date = datetime.strptime(source_data.last_update_date, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Incorrect date format. Date format should be 'YYYY-MM-DD HH:MM:SS'.")
+
+    if not source_data.frequency[:-1].isdigit():
+        raise HTTPException(status_code=400, detail="Incorrect frequency format. Frequency format should be a integer followed by M.")
+    if not source_data.frequency[-1]=="M":
+        raise HTTPException(status_code=400, detail="Incorrect frequency format. Frequency format should be a integer followed by M.")
+
+    conn = connect_to_database()
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO source_data (source, source_tag, source_type, from_date, to_date, last_update_date, frequency) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            (source_data.source, source_data.source_tag, source_data.source_type, from_date, to_date, last_update_date, source_data.frequency)
+        )
+        conn.commit()
+        return {"status": "success"}
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail="Error adding data to the database.") from e
+    finally:
+        conn.close()
 
 # Create the table on startup
 create_table()
+
+
 
 
 # Close the database connection when the application stops
